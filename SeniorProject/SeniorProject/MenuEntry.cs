e5d1b952e75a1fc8b864ff309cb579a0c468d439
skrizo88 <skrizo88@d845c018-14ff-11df-af62-8f85de635dbf>
2010-04-07 01:40:02 +0000

    Last known save of working objects...just in case i lose my hard drive in the near future

---
+++ SeniorProject/SeniorProject/MenuEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SeniorProject
+{
+    public class MenuEntry
+    {
+        #region Variables
+        SpriteFont gameFont;
+        public string text;
+        private const string MENU = "Menu";
+        private const string ITEM = "Items";
+        #endregion
+        public void LoadContent(ContentManager theContentManager)
+        {
+           gameFont = theContentManager.Load<SpriteFont>("SpriteFont1");     //loads a font
+        }
+
+        public MenuEntry(string text)
+        {
+            this.text = text;
+        }
+
+        public virtual void Draw(GameTime gameTime, Vector2 position, SpriteBatch spritebatch, bool isSelected, Color color)
+        {
+            if (isSelected)
+            {
+                spritebatch.DrawString(gameFont, text, position, Color.Fuchsia, 0, Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+            }
+            else
+            {
+                spritebatch.DrawString(gameFont, text, position, color, 0, Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+            }
+        }
+
+
+    }
+}
